c2fb30bfe7de98963e3203fabe28a0a1f48daa3b
admakinh <admakinh@gmail.com>
2022-05-24 14:29:25 -0400

    display time collect input and display search history using local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,55 @@ var currentFiveDayEl = document.querySelector("#current-five-day-el");
 var searchHistoryEl = document.querySelector("#search-hist-el");
 
 
+// function to display time
+function getTime() {
+    var currentTime = moment().format('MMM DD YYYY [at] hh:mm:ss a');
+    // $("#time").text("Local Time: " + currentTime)
+    displayTimeEl.textContent = "Current Local Time: " + currentTime;
+}
+// performs the funtion every second
+setInterval(getTime, 1000);
+
+// create an empty array to store cities in
+var cities = [];
+
+var formSubmit = function (event) {
+    event.preventDefault();
+    var searchedCity = searchInputEl.value.trim();
+    if (searchedCity) {
+        cities.unshift({ searchedCity });
+        searchInputEl.value = "";
+    } else {
+        alert("Enter a city to search");
+    }
+    saveSearch();
+    pastSearch(searchedCity);
+}
+
+var saveSearch = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+
+// display search history
+var pastSearch = function (pastSearch) {
+    searchHistButton = document.createElement("button");
+    searchHistButton.textContent = pastSearch;
+    searchHistButton.classList = "d-flex w-120 btn-light border p-2";
+    searchHistButton.setAttribute("data-city", pastSearch)
+    searchHistButton.setAttribute("type", "submit");
+    searchHistoryEl.prepend(searchHistButton);
+}
+// load on refresh
+var loadOnRefresh = function () {
+    var pastCities = JSON.parse(localStorage.getItem("cities"))
+    
+    for (let i = 0; i < pastCities.length; i++) {
+        pastSearch(pastCities[i].city)
+      }
+    return
+}
+
+// submission listener
+searchFormEl.addEventListener("submit", formSubmit);
+loadOnRefresh();
