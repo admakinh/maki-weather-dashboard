66b23fe2acbc7f89d30572f6a33edca1cee7db27
admakinh <admakinh@gmail.com>
2022-05-24 13:44:46 -0400

    layout basic structure and variables

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// api URL + key
+var apiKey = "b6f060c7bda2e8f96d21a9c0d4ad8f20";
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// vars
+var searchFormEl = document.querySelector("#")
+var searchInputEl = document.querySelector("#");
+
+var currentWeatherEl = document.querySelector("#");
+var displayedCityEl = document.querySelector("#");
+var fiveDayTitleEl = document.querySelector("#");
+var currentFiveDayEl = document.querySelector("#");
+
+var searchHistoryEl = document.querySelector("#");
+
+var displayTimeEl = document.querySelector("#");
+
